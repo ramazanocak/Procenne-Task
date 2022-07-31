4f14d0cec853b6d86ba37cc0208ccb6cc7261278
ramazanocak <31711477+ramazanocak@users.noreply.github.com>
2022-07-31 22:12:22 +0300

    .xcframework is created
    .xcframework is created both simulator and device

---
+++ ProcenneTech_case-iphonesimulator.xcarchive/Products/Library/Frameworks/ProcenneTech_case.framework/Headers/ProcenneTech_case.h
@@ -0,0 +1,16 @@
+//
+//  ProcenneTech_case.h
+//  ProcenneTech_case
+//
+//  Created by Ramazan Ocak on 30.07.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ProcenneTech_case.
+FOUNDATION_EXPORT double ProcenneTech_caseVersionNumber;
+
+//! Project version string for ProcenneTech_case.
+FOUNDATION_EXPORT const unsigned char ProcenneTech_caseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProcenneTech_case/PublicHeader.h>
